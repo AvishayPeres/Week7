f4509f735691ee1867b15f94905c6b2eed36e5b1
Avishay Peres <avishay.peres@gmail.com>
2019-04-22 10:57:09 +0300

    Added ex.1

---
+++ Day2/main.js
@@ -0,0 +1,20 @@
+class UniqueArray = {
+
+add(item)
+showAll()
+exists(item)
+get(index)
+}
+
+
+
+
+const uniqueStuff = new UniqueArray()
+uniqueStuff.add("toy")
+uniqueStuff.showAll() //prints ["toy"]
+uniqueStuff.add("toy")
+uniqueStuff.showAll() //prints ["toy"]
+uniqueStuff.exists("toy") //returns true
+uniqueStuff.add("poster")
+uniqueStuff.add("hydrogen")
+console.log(uniqueStuff.get(2)) //prints "hydrogen"
