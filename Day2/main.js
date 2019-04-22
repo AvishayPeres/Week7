297f63798b882b52249e87640d6302ee548b7d2e
Avishay Peres <avishay.peres@gmail.com>
2019-04-22 11:55:41 +0300

    finised ex.1

--- Day2/main.js
+++ Day2/main.js
@@ -1,20 +1,53 @@
-class UniqueArray = {
+class UniqueArray {
+    constructor() {
+        this.arr = []
+        this.length = 0
+        this.obj = {}
+    }
 
-add(item)
-showAll()
-exists(item)
-get(index)
+    _isPrimitive(item) {
+        return (item !== Object(item))
+    }
+
+    add(item) {
+        if(this._isPrimitive) {
+            if(!this.exists(item)) {
+                this.arr[this.length] = item
+                this.length++
+                this.obj[item] = 1
+            }
+        }
+    }
+
+    showAll() {
+        console.log(this.arr)
+    }
+
+    exists(item) {
+        if(this.obj[item]){
+            return true
+        } else {
+            return false
+        }
+    }
+
+    get(index) {
+        if(index <= (this.length - 1)){
+            return this.arr[index]
+        } else {
+            return -1
+        }
+    }
+
 }
 
-
-
-
 const uniqueStuff = new UniqueArray()
+
 uniqueStuff.add("toy")
 uniqueStuff.showAll() //prints ["toy"]
 uniqueStuff.add("toy")
 uniqueStuff.showAll() //prints ["toy"]
-uniqueStuff.exists("toy") //returns true
+console.log(uniqueStuff.exists("toy")) //returns true
 uniqueStuff.add("poster")
 uniqueStuff.add("hydrogen")
 console.log(uniqueStuff.get(2)) //prints "hydrogen"
