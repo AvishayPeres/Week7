b5ba55a619cfe1f73f5e84fc8f5f224af9049905
Avishay Peres <avishay.peres@gmail.com>
2019-04-23 12:25:04 +0300

    Recursion Ex.3

--- Day3/main.js
+++ Day3/main.js
@@ -8,9 +8,24 @@
 
 // console.log (factorial(5))
 
-const reverString = function (string){
-    if(string === ""){return string}
+// const reverString = function (string){
+//     if(string === ""){return string}
      
-        return reverString(string.substr(1)) + string[0]
+//         return reverString(string.substr(1)) + string[0]
+// }
+//    console.log (reverString("avishay"))
+
+
+const arr1 = [1, 2, 3]
+const arr2 = []
+
+const swap = function (arr1,arr2){
+    if(arr1.length === 0){return arr1,arr2}
+        arr2.push(arr1.splice(0,1))
+
+    return swap(arr1,arr2)
 }
-   console.log (reverString("avishay"))
+
+swap(arr1, arr2)
+console.log(arr1) //[]
+console.log(arr2) //[1, 2, 3]
