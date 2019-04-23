8a14fe16b7565df22349588f1e8f6204997636c2
Avishay Peres <avishay.peres@gmail.com>
2019-04-23 12:03:32 +0300

    Recursion Ex.2

--- Day3/main.js
+++ Day3/main.js
@@ -1,9 +1,16 @@
-const factorial = function (number, sum=number) {
-    if (number === 1) { return sum }
-        sum *= number - 1
-        number--
+// const factorial = function (number, sum=number) {
+//     if (number === 1) { return sum }
+//         sum *= number - 1
+//         number--
 
-    return factorial(number, sum)
+//     return factorial(number, sum)
+// }
+
+// console.log (factorial(5))
+
+const reverString = function (string){
+    if(string === ""){return string}
+     
+        return reverString(string.substr(1)) + string[0]
 }
-
-console.log (factorial(5))
+   console.log (reverString("avishay"))
