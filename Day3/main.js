f7fac2fab60e490f4beba74ab6a3b0a652e14b4f
Avishay Peres <avishay.peres@gmail.com>
2019-04-24 13:30:27 +0300

    Ex.3 Finished

--- Day3/main.js
+++ Day3/main.js
@@ -162,17 +162,92 @@ class Matrix {
     get(r, c) {
         return get(this.matrix, r, c)
     }
+    // // ------------Ex.2----- to add '    findCoordinate'
+
+    findCoordinate(value) {
+        for (let i = 0; i < this.matrix.length; i++) {
+            for (let j = 0; j < this.matrix[i].length; j++) {
+                if (this.matrix[i][j] === value) {
+                    return { x: j, y: i }
+                }
+            }
+        }
+    }
 }
 
-let m = new Matrix(3, 4)
-m.print()
-//prints
-/*
-1       2       3       4
-5       6       7       8
-9       10      11      12
-*/
+    
 
-m.alter(0, 0, m.get(1, 1))
-m.printColumn(0) //prints 6, 5, 9 (separate lines)
-m.printRow(0) //prints 6, 2, 3, 4 (separate lines)
+// let m = new Matrix(3, 4)
+// m.print()
+// //prints
+// /*
+// 1       2       3       4
+// 5       6       7       8
+// 9       10      11      12
+// */
+
+// m.alter(0, 0, m.get(1, 1))
+// m.printColumn(0) //prints 6, 5, 9 (separate lines)
+// m.printRow(0) //prints 6, 2, 3, 4 (separate lines)
+
+
+// ------------Ex.3-----
+
+class EmployeeMatrix extends Matrix {
+    constructor() {
+        super()
+    }
+
+    loadData(salaryData) {
+        for (let salary of salaryData) {
+            this.matrix.push([salary._id, salary.name, salary.department, salary.salary])
+        }
+    }
+
+    getEmployees(department) {
+        let depArr = []
+        for (let i = 0; i < this.matrix.length; i++) {
+            if (this.matrix[i][2] == department) {
+                depArr.push(this.matrix[i][1])
+            }
+        }
+        return depArr
+    }
+
+    getTotalSalary(department) {
+        let depSal = 0
+        for (let i = 0; i < this.matrix.length; i++) {
+            if (this.matrix[i][2] == department) {
+                depSal += this.matrix[i][3]
+            }
+        }
+        return depSal
+    }
+
+    findRichest() {
+        let maxSal = 0
+        let richest = ""
+        for (let i = 0; i < this.matrix.length; i++) {
+            if (this.matrix[i][3] > maxSal) {
+                maxSal = this.matrix[i][3]
+                richest = this.matrix[i][1]
+            }
+        }
+        return richest
+    }
+}
+let data = [
+    { _id: "e10021", name: "Gillian", department: "Finance", salary: 2000 },
+    { _id: "e10725", name: "Tibor", department: "Design", salary: 1200 },
+    { _id: "e10041", name: "Anisha", department: "Finance", salary: 2300 },
+    { _id: "e10411", name: "Jakub", department: "Design", salary: 1600 },
+    { _id: "e11995", name: "Mar", department: "Design", salary: 1300 },
+    { _id: "e10732", name: "Nisha", department: "Design", salary: 1200 }
+]
+
+let m = new EmployeeMatrix()
+
+m.loadData(data)
+m.print()
+
+
